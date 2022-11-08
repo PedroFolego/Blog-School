21486445acdba7b3b747ae53ba9d56df773eaed0
Pedro Folego <hmfpedro@hotmail.com>
2022-11-07 23:52:20 -0300

    feat: add base test

--- src/Blog-School.Test/BlogContextTest.cs
+++ src/Blog-School.Test/BlogContextTest.cs
@@ -13,4 +13,17 @@ public class BlogContextTest : DbContext, IBlogContext
   public DbSet<Student> Students { get; set; }
   public DbSet<Post> Posts { get; set; }
 
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+  {
+    if (!optionsBuilder.IsConfigured)
+    {
+      optionsBuilder.UseSqlServer(@"
+        Server=127.0.0.1;
+        Database=blog_test;
+        User=SA;
+        Password=Password12;
+      ");
+    }
+  }
+
 }
